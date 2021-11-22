9328d1b8745d5beea3dd2cf93b7686ba654867ce
ivankao <ivanjo39191@gmail.com>
2021-11-22 14:57:03 +0800

    pandas_visual_analysis
    pandas_visual_analysis

---
+++ pandas_visual_analysis.py
@@ -0,0 +1,8 @@
+pip install pandas-visual-analysis
+pip install openpyxl
+
+import pandas as pd
+from pandas_visual_analysis import VisualAnalysis
+df = pd.read_excel('excel_data (82).xlsx')
+df
+VisualAnalysis(df)
